cca32297cde8334eb434cb167efa5b1bfa136609
TYA_IOS <appadmin@segio.com>
2024-09-26 16:28:55 +0900

    Initial Commit

---
+++ ComposableReducerDemo/ComposableReducerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableReducerDemoApp.swift
+//  ComposableReducerDemo
+//
+//  Created by NewUser on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ComposableReducerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
