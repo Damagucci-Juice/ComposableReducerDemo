910f468ede65bdb2c84b4baf4f629c9d1b1ca8a9
TYA_IOS <appadmin@segio.com>
2024-09-27 09:51:26 +0900

    Level4: Composing Features

--- ComposableReducerDemo/ComposableReducerDemoApp.swift
+++ ComposableReducerDemo/ComposableReducerDemoApp.swift
@@ -9,14 +9,14 @@ import SwiftUI
 
 @main
 struct ComposableReducerDemoApp: App {
-    static let store = Store(initialState: CounterFeature.State()) {
-        CounterFeature()
+    static let store = Store(initialState: AppFeature.State()) {
+        AppFeature()
             ._printChanges()
     }
 
     var body: some Scene {
         WindowGroup {
-            CounterView(store: ComposableReducerDemoApp.store)
+            AppView(store: ComposableReducerDemoApp.store)
         }
     }
 }
